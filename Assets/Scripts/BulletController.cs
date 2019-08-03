46e26a2d8fe3b15f094e9116e572e35c7bf3c749
Alexander Wohlfahrt <alexander.wohlfahrt@he-arc.ch>
2019-08-03 12:51:48 +0200

    Create base physics of the bullet (rebound, sprites orientation)

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    [SerializeField] private float initSpeed = 0;
+
+    private Rigidbody2D rigid2d;
+
+    private void Start()
+    {
+        rigid2d = GetComponent<Rigidbody2D>();
+        rigid2d.velocity = transform.up * initSpeed;
+    }
+
+    private void Update()
+    {
+        float radian = Mathf.Atan2(rigid2d.velocity.normalized.y, rigid2d.velocity.normalized.x);
+        Debug.Log("r " + radian);
+        float degree = radian * Mathf.Rad2Deg;
+        Debug.Log("d " + degree);
+
+        float finalDegree = degree - 90;
+        Debug.Log("fd " + finalDegree);
+
+        transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, transform.rotation.y, finalDegree));
+    }
+}
