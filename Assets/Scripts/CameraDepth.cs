a9f23b3020a32cafbc37b74371bde87fc7bfd19e
Etienne Hüsler <etienne.husler@he-arc.ch>
2019-08-03 11:45:44 +0200

    Responsive maps

---
+++ Assets/Scripts/CameraDepth.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CameraDepth : MonoBehaviour
+{
+    private void Update()
+    {
+        // Aspect ratio of the screen
+        float ratio = (float)Screen.width / Screen.height;
+
+        // Depth of the camera based on an online quadratic regression with some sample points
+        float depth = -79.38451f + 50.33977f * ratio - 10.59345f * ratio * ratio;
+
+        // Update the depth of the camera
+        transform.position = new Vector3(transform.position.x, transform.position.y, depth);
+    }
+}
