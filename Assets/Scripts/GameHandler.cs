1bd104d618d62c60a36eff9a70399139b0790bb8
Etienne Hüsler <etienne.husler@he-arc.ch>
2019-08-03 16:08:03 +0200

    Spawn slimes

--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -5,49 +5,98 @@ using UnityEngine;
 public class GameHandler : MonoBehaviour
 {
     [SerializeField] private GameObject levelsContainer;
+    [SerializeField] private Sprite slimeSprite;
 
-    private GameObject[] levels;
+    private GameObject[] levelsObjects;
+    private Level[] levels;
+    private List<List<(float, Slime)>> mobs;
     private int levelIndex;
+    private float initTime;
     
     private void Start()
     {
         levelIndex = 0;
+        initTime = Time.time;
 
+        FillSlimes();
         FillLevels();
         ActivateLevel();
     }
 
+    private void FillSlimes()
+    {
+        mobs = new List<List<(float, Slime)>>
+        {
+            new List<(float, Slime)>
+            {
+                (2, new Slime(0, 0, 0.5f, 0, slimeSprite)),
+                (4, new Slime(0, 0, 0.5f, 0, slimeSprite)),
+                (6, new Slime(0, 0, 0.5f, 0, slimeSprite)),
+                (8, new Slime(0, 0, 0.5f, 0, slimeSprite))
+            },
+            new List<(float, Slime)>
+            {
+                (2, new Slime(0, 0, 0.5f, 0, slimeSprite)),
+                (4, new Slime(0, 0, 0.5f, 0, slimeSprite)),
+                (6, new Slime(0, 0, 0.5f, 0, slimeSprite)),
+                (8, new Slime(0, 0, 0.5f, 0, slimeSprite))
+            }
+        };
+    }
+
     private void FillLevels()
     {
-        levels = new GameObject[levelsContainer.transform.childCount];
-        for (int i = 0; i < levels.Length; i++)
+        // Lists
+        levelsObjects = new GameObject[levelsContainer.transform.childCount];
+        levels = new Level[levelsContainer.transform.childCount];
+
+        for (int i = 0; i < levelsObjects.Length; i++)
         {
-            levels[i] = levelsContainer.transform.GetChild(i).gameObject;
+            // Level (Object)
+            levelsObjects[i] = levelsContainer.transform.GetChild(i).gameObject;
+
+            // Fill spawns
+            GameObject spawnsObject = levelsObjects[i].transform.Find("Spawns").gameObject;
+            GameObject[] spawns = new GameObject[spawnsObject.transform.childCount];
+            for (int j = 0; j < spawnsObject.transform.childCount; j++)
+            {
+                spawns[j] = spawnsObject.transform.GetChild(j).gameObject;
+            }
+
+            // Level (Script)
+            levels[i] = new Level(spawns, mobs[i]);
         }
     }
 
     private void ActivateLevel()
     {
-        for (int i = 0; i < levels.Length; i++)
+        for (int i = 0; i < levelsObjects.Length; i++)
         {
-            levels[i].SetActive(i == levelIndex);
+            levelsObjects[i].SetActive(i == levelIndex);
         }
     }
 
     private void Update()
     {
         HandleKeys();
+        UpdateLevel();
+    }
+
+    private void UpdateLevel()
+    {
+        Level activeLevel = levels[levelIndex];
+        activeLevel.Update(Time.time - initTime);
     }
 
     private void HandleKeys()
     {
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            levelIndex = (levelIndex - 1 + levels.Length) % levels.Length;
+            levelIndex = (levelIndex - 1 + levelsObjects.Length) % levelsObjects.Length;
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            levelIndex = (levelIndex + 1) % levels.Length;
+            levelIndex = (levelIndex + 1) % levelsObjects.Length;
         }
         ActivateLevel();
     }
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level
+{ 
+    private GameObject[] spawns;
+    private List<(float, Slime)> mobs;
+
+    public Level(GameObject[] spawns, List<(float, Slime)> mobs)
+    {
+        this.spawns = spawns;
+        this.mobs = mobs;
+    }
+
+    public void Update(float timePassed)
+    {
+        if (mobs.Count > 0)
+        {
+            (float, Slime) slime = mobs[0];
+            if (slime.Item1 <= timePassed)
+            {
+                slime.Item2.Instantiate(NextSpawn());
+                mobs.RemoveAt(0);
+            }
+        }
+    }
+
+    private Vector3 NextSpawn()
+    {
+        GameObject spawn = spawns[Random.Range(0, mobs.Count)];
+        return spawn.transform.position;
+    }
+}
