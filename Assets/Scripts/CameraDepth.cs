a933262b7be782858c8de19491d841d9630b36bd
Etienne Hüsler <etienne.husler@he-arc.ch>
2019-08-03 12:39:39 +0200

    Improved responsivity

--- Assets/Scripts/CameraDepth.cs
+++ Assets/Scripts/CameraDepth.cs
@@ -2,10 +2,12 @@
 
 public class CameraDepth : MonoBehaviour
 {
+    private const float ASPECT_RATIO = 16f / 9f;
+
     private void Update()
     {
         // Aspect ratio of the screen
-        float ratio = (float)Screen.width / Screen.height;
+        float ratio = Mathf.Min((float)Screen.width / Screen.height, ASPECT_RATIO);
 
         // Depth of the camera based on an online quadratic regression with some sample points
         float depth = -79.38451f + 50.33977f * ratio - 10.59345f * ratio * ratio;
