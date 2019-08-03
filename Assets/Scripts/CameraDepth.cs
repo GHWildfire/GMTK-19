30159c9a78887a97a6f879ac13dbb7cda19b47aa
Etienne Hüsler <etienne.husler@he-arc.ch>
2019-08-03 23:31:52 +0200

    Responsive orthographic projection

--- Assets/Scripts/CameraDepth.cs
+++ Assets/Scripts/CameraDepth.cs
@@ -9,10 +9,7 @@ public class CameraDepth : MonoBehaviour
         // Aspect ratio of the screen
         float ratio = Mathf.Min((float)Screen.width / Screen.height, ASPECT_RATIO);
 
-        // Depth of the camera based on an online quadratic regression with some sample points
-        float depth = -79.38451f + 50.33977f * ratio - 10.59345f * ratio * ratio;
-
-        // Update the depth of the camera
-        transform.position = new Vector3(transform.position.x, transform.position.y, depth);
+        // Orthographic size of the camera based on an online quadratic regression with some sample points
+        GetComponent<Camera>().orthographicSize = 49.92087f - 34.0246f * ratio + 7.616427f * ratio * ratio;
     }
 }
